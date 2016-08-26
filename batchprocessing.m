249def3f271c2d69c4342fd3f33b7827e1b051aa
Papiertig0r <christopher.putz@sheep-records.de>
2016-08-26 14:05:50 +0200

    Changed quotation marks to octave preferred ones

--- batchprocessing.m
+++ batchprocessing.m
@@ -1,22 +1,22 @@
 function batchprocessing(folder, force_overwrite = false)
   more off;
-  files = dir(strcat(folder, "*.csv"));
-  disp(sprintf("Detected %i files.", length(files)));
+  files = dir(strcat(folder, '*.csv'));
+  disp(sprintf('Detected %i files.', length(files)));
   numberOfNewFiles = 0;
   for i=1:length(files)
     j = 0;
     
-    if(!force_overwrite && file_in_path(folder, strcat(files(i).name, ".png")) != 0)
+    if(!force_overwrite && file_in_path(folder, strcat(files(i).name, '.png')) != 0)
       continue
     endif
     
     try 
       do
         j++;     
-          data=dlmread(strcat(folder,files(i).name), ";");
+          data=dlmread(strcat(folder,files(i).name), ';');
       until (data(j, 1) > 0)
     catch
-      disp(sprintf("Error in %s, continuing", files(i).name))
+      disp(sprintf('Error in %s, continuing', files(i).name))
       continue
     end_try_catch
     
@@ -28,11 +28,11 @@ function batchprocessing(folder, force_overwrite = false)
       continue
     endif
     
-    saveas(1, strcat(folder, files(i).name, ".png"), "png");
+    saveas(1, strcat(folder, files(i).name, '.png'), 'png');
     close all;
     numberOfNewFiles++;
     
   endfor
-  disp(sprintf("%i new files written, %i skipped.", numberOfNewFiles, length(files) - numberOfNewFiles));
+  disp(sprintf('%i new files written, %i skipped.', numberOfNewFiles, length(files) - numberOfNewFiles));
   more on;
 endfunction
